357a19a3f4fda6456b55475045ba038cb2b36c97
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-06-11 18:07:19 +0300

    Revert "Merge pull request #1 from F1sxher/main"
    This reverts commit e773bc952b0a8f1e998330286c3ebb7988bf0372, reversing
    changes made to 87c0af05eac2eb3822585edb08f51cf1e3023d0d.

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 A re-work of the Powercord plugin [pc-hastebin](https://github.com/powercord-org/powercord/tree/v2/src/Powercord/plugins/pc-hastebin) to be used with [Micro](https://github.com/sylv/micro).
 
 **To-do**
-
-- [x] Add an input for expiry
-- [x] Clipboard by default setting
-- [x] Implement the toggles for the flags
+- [ ] Add an input for expiry
+- [ ] Clipboard by default setting
+- [ ] Implement the toggles for the flags
--- Settings.jsx
+++ Settings.jsx
@@ -1,40 +1,5 @@
 const { React } = require('powercord/webpack');
-const { TextInput, SwitchItem, SelectInput } = require('powercord/components/settings');
-
-const TYPE_OPTIONS = [
-  { label: "Markdown", value: "md" },
-  { label: "Plain Text", value: "txt" },
-  { label: "HTML", value: "html" },
-  { label: "JSON", value: "json" },
-  { label: "XML", value: "xml" },
-  { label: "SQL", value: "sql" },
-  { label: "JavaScript", value: "js" },
-  { label: "TypeScript", value: "ts" },
-  { label: "JSX", value: "jsx" },
-  { label: "TSX", value: "tsx" },
-  { label: "CSS", value: "css" },
-  { label: "SCSS", value: "scss" },
-  { label: "SASS", value: "sass" },
-  { label: "LESS", value: "less" },
-  { label: "GraphQL", value: "graphql" },
-  { label: "C", value: "c" },
-  { label: "C++", value: "cpp" },
-  { label: "C#", value: "cs" },
-  { label: "Python", value: "py" },
-  { label: "R", value: "r" },
-  { label: "Ruby", value: "rb" },
-  { label: "Shell", value: "sh" },
-  { label: "Java", value: "java" },
-  { label: "Kotlin", value: "kt" },
-  { label: "Go", value: "go" },
-  { label: "Swift", value: "swift" },
-  { label: "Rust", value: "rs" },
-  { label: "YAML", value: "yaml" },
-  { label: "PHP", value: "php" },
-  { label: "Perl", value: "pl" },
-  { label: "PowerShell", value: "ps1" },
-  { label: "Batch", value: "bat" },
-];
+const { TextInput, SwitchItem } = require('powercord/components/settings');
 
 module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
   <div>
@@ -47,50 +12,19 @@ module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
       Domain
     </TextInput>
     <TextInput
-      note='The authorization header name for your domain.'
-      defaultValue={getSetting('authHeaderName', 'Authorization')}
-      required={false}
-      onChange={val => updateSetting('authHeaderName', val)}
-    >
-      Auth Header Name
-    </TextInput>
-    <TextInput
-      note='The authorization key for your domain.'
+      note='The auth key for your domain.'
       defaultValue={getSetting('authKey', '')}
       required={false}
       onChange={val => updateSetting('authKey', val)}
     >
       Auth Key
     </TextInput>
-    <TextInput
-      note='Number of hours until it expires.'
-      defaultValue={getSetting('expiry', 24)}
-      required={false}
-      onChange={val => updateSetting('expiry', val)}
-    >
-      Expiration
-    </TextInput>
-    <SelectInput
-      note='Default File/Markdown Extension'
-      value={getSetting('ext', 'md')}
-      options={TYPE_OPTIONS}
-      required={true}
-      onChange={opt => updateSetting('ext', opt.value)}
-    >Default Extension
-    </SelectInput>
     <SwitchItem
       note='Whether the link is sent in chat by default or not.'
       value={getSetting('send', false)}
       onChange={() => toggleSetting('send')}
     >
       Send Link
-    </SwitchItem>
-    <SwitchItem
-      note='Whether the command grabs from clipboard automatically'
-      value={getSetting('clip', true)}
-      onChange={() => toggleSetting('clip')}
-    >
-      Clipboard
     </SwitchItem>
     <SwitchItem
       note='Whether to encrypt the paste content, requiring the encryption key to be in the url'
